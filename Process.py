f67bcba026d106a71ba853eea0aba47a65e18a4b
Wolfy Fiorini <jonwolffiorini@gmail.com>
2022-05-02 00:21:50 -0600

    added stuff to help interpolate smoother

--- Process.py
+++ Process.py
@@ -2,6 +2,14 @@ import requests
 from PIL import Image
 from PIL import ImageFilter
 
+def process(input, out) : 
+    image = scale(input, out)
+    image = crop(image)
+    image = sharpen(image, 2)
+    image.save(out)
+
+
+
 def scale(input, out) :
     r = requests.post(
         "https://api.deepai.org/api/waifu2x",
@@ -34,3 +42,49 @@ def sharpen(image, passes) :
         sharpened1 = im.filter(ImageFilter.SHARPEN)
         im = sharpened1
     return im
+
+def resize(image, height, width) :
+    image = Image.open(input)
+    image = image.resize((height, width))
+    image.save(input)
+    return
+
+def fade(images, num_between, files) :
+    new_files = []
+
+    for file in files :
+        if images.__contains__(file) :
+            im1 = Image.open(file)
+            im2 = Image.open(images[file])
+            name = images[file]
+            name = name.replace(".jpg", "")
+            num = findnum(name)
+            print(num)
+            if num == "1" :
+                name = name.replace("Temp1-0", "")
+                num = findnum(file)
+                print("\n\n 1 found\n\n")
+                print(num)
+            name = name.replace("Temp{}-0".format(num), "")
+            name += "Temp" + num
+            save_name = name
+            alpha = 1
+            print("blending " + file + " with " + images[file])
+            for i in range(num_between) :
+                name = name + "-{}.jpg".format(i + 1)
+                alpha -= 1 / (num_between)
+                print(alpha)
+                blend = Image.blend(im1, im2, alpha)
+                blend.save(name)
+                new_files.append(name)
+                name = save_name
+            new_files.append(file)
+        else :
+            new_files.append(file)
+    return new_files
+
+def findnum(name) :
+    split = name.split("Temp")
+    nums = split[1]
+    nums = nums.split("-")
+    return nums[0]
