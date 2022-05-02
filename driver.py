09f493a6a59448e85a3ffcbfbd86a6dd67022494
Wolfy Fiorini <jonwolffiorini@gmail.com>
2022-05-02 00:24:56 -0600

    added video generation and some stuff i might use

--- driver.py
+++ driver.py
@@ -1,8 +1,12 @@
+import enum
 import glob
 import shutil
 import NST
 import Process
+import subprocess
 import os
+import re
+from threading import Timer
 from PIL import Image
 import myconfig as prefs
 
@@ -110,6 +114,12 @@ else :
 temp_folder = NST.create_results_folder("temp")
 style_path = "Style/" + input("Enter style folder - ") + "/"
 style_files = input("Enter style files - ")
+# content_path = "Content/" + input("enter content folder - ") + "/"
+# content_files = input("enter content files - ")
+# content_files = content_files.split(" ")
+# for i, file in enumerate(content_files) :
+# 	file_name = content_path + content_files[i] + ".jpg"
+# 	content_files[i] = file_name
 if style_files != "" :
 	style_files = style_files.split(" ")
 	for i, file in enumerate(style_files) :
@@ -131,10 +141,46 @@ NST.imgHeight = int(NST.imgWidth / ratio // 1)
 
 print("Transferring styles... ")
 files = NST.run_styles(temp_folder, style_files, fr, content_img)
+# remove_string = input("Enter file number of any styles you would like to delete - ")
+# if not remove_string == "" :
+# 	remove = remove_string.split(" ")
+# 	num_styles, files = remove_files(remove, num_styles, files)
 
 files = rename_files(temp_folder, fr, files, True)
 upscale_imgs(files)
 
+# generate_video()
+# 
+# files = NST.run_interp(temp_folder, num_styles, form, fr, files, steps)
+# style_frame_files[files[len(files) - 1]] = temp_folder + "/Temp1-0.jpg"
+# print(style_frame_files)
+# Process.fade(style_frame_files, fr//num_styles, files[len(files) - 1])
+# print("\n\n\n")
+# print("Renaming files...")
+# rename_files(temp_folder, fr, False)
+# files = glob.glob(temp_folder + "/*.jpg")
+# files.sort(key = my)
+# input(".......")
+
+
+# print("Generating video... ")
+# loops = (duration * fr) // len(files)
+# os.chdir(r)
+
+# for i in range(loops) :
+# 	ffmpeg = "ffmpeg -framerate " + str(fr) + " -i Frame%d.jpg -c:v libx264 "+ out_name + "{}.mp4".format(i) 
+# 	os.system(ffmpeg)
+
+
+# os.system("(for %i in (*.mp4) do @echo file '%i') > mylist.txt")
+# ffmpeg = "ffmpeg -f concat -safe 0 -i mylist.txt -c copy "+ out_name + ".mp4"
+# os.system(ffmpeg)
+
+
+
+# shutil.move(r + "/" + out_name + ".mp4", r + "/Video/" + out_name + ".mp4")
+# # os.remove(results_folder + "*.mp4")
+# shutil.rmtree(temp_folder)
 
 
 
